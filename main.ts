1c3af4688547530f30c53f2f593bb1b29ce736d8
aidr669 <132546582+aidr669@users.noreply.github.com>
2023-05-26 12:37:32 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,21 @@
-
+radio.onReceivedNumber(function (receivedNumber) {
+    led.setBrightness(255)
+})
+radio.onReceivedString(function (receivedString) {
+    if (receivedString == "power") {
+        entendido = !(entendido)
+        led.setBrightness(125)
+        led.enable(entendido)
+    }
+})
+let entendido = false
+radio.setGroup(1)
+entendido = false
+led.enable(entendido)
+basic.showLeds(`
+    # # # # #
+    # # # # #
+    # # # # #
+    # # # # #
+    # # # # #
+    `)
